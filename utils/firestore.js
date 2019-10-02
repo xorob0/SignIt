cccd7e31bbbb59b7ad6596021d155bfb83ec6726
Timothée Simon <timothee@hackages.io>
2019-10-02 20:53:37 +0200

    Added firebase project

---
+++ utils/firestore.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+require('firebase/firestore');
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2zBzfHI7ekXXjjayg7f9x-JnVA5vsgqA',
+  authDomain: 'signin-test-7500f.firebaseapp.com',
+  databaseURL: 'https://signin-test-7500f.firebaseio.com',
+  projectId: 'signin-test-7500f',
+  storageBucket: 'signin-test-7500f.appspot.com',
+  messagingSenderId: '215992674591',
+  appId: '1:215992674591:web:a405357d05238a549a682a',
+};
+
+export const firestore = firebase.firestore();
